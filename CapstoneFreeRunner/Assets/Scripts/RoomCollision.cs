fea1b8ea7a6d0edaaafc02ccbd228f1ab0e55e32
Sam Kern <samkern108@gmail.com>
2015-10-30 11:40:44 -0600

    Updated .gitignore to ignore userprefs, Added rooms and temporary sprites, Made rooms disappear when the user is outside of that room.

---
+++ CapstoneFreeRunner/Assets/Scripts/RoomCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomCollision : MonoBehaviour {
+
+	void OnTriggerEnter2D()
+	{
+		SpriteRenderer s = this.GetComponent ("SpriteRenderer") as SpriteRenderer;
+		s.enabled = true;
+		foreach (Transform child in transform.parent)
+		{
+			if (child != transform.parent && child != this.transform){
+				SpriteRenderer r = child.gameObject.GetComponent("SpriteRenderer") as SpriteRenderer;
+				r.enabled = false;
+			}
+		}
+	}
+}
