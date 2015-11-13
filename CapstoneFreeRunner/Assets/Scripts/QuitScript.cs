672e474fef9964e09e027be7b9931696797fd756
Richard Bui <rchrd.bui@gmail.com>
2015-11-13 13:12:39 -0700

    So here are my  GUI things.  I placed them in separate scenes just as you asked.  Let me know if any thing is missing so I can quickly fix it (or at least go to the presenting computer to fix it).

---
+++ CapstoneFreeRunner/Assets/Scripts/QuitScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitScript : MonoBehaviour {
+    public void OnQuit()
+    {
+        Application.Quit(); //this quits the game
+    }
+}
