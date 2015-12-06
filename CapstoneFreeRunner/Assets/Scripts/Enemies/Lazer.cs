e8a3579d1eae60c1cb42c4d74da83cbfa14fab3a
James Clow <jameclow@cs.du.edu>
2015-12-06 16:23:12 -0700

    Lazers are in

---
+++ CapstoneFreeRunner/Assets/Scripts/Enemies/Lazer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lazer : MonoBehaviour {
+
+	LineRenderer LR;
+	Transform DirectionHandle;
+	bool active;
+	float maxLazerLength = 10000;
+	Color startColor = Color.red;
+	Color endColor = Color.red;
+	// Use this for initialization
+	void Start () {
+		LR = gameObject.GetComponent<LineRenderer>();
+		DirectionHandle = transform.Find("DirectionHandle");
+		active = true;
+
+		LR.material = new Material(Shader.Find("Particles/Additive"));
+		LR.SetColors(startColor, endColor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active){
+			Vector3 dir = (DirectionHandle.position - transform.position).normalized;		
+			Ray2D ray = new Ray2D(transform.position, dir);
+			RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, maxLazerLength);
+
+			//draw lazer
+			LR.SetPosition(0, ray.origin);
+			if(hit == true){
+				LR.SetPosition(1,hit.point);
+			}else{
+				LR.SetPosition(1,ray.GetPoint(maxLazerLength));
+			}
+
+			//if player enters lazer
+			if (hit.collider.gameObject.tag == "Player"){
+				hit.collider.gameObject.SendMessage("KillPlayer");
+			}
+		}
+	}
+}
---
+++ CapstoneFreeRunner/Assets/Scripts/Enemies/LinearObjectTrack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinearObjectTrack : MonoBehaviour {
+
+	Transform obj;
+	Transform pointA;
+	Transform pointB;
+	float speed = 1;
+	// Use this for initialization
+	void Start () {
+		obj = transform.GetChild(2);
+		pointA = transform.Find("PointA");
+		pointB = transform.Find("PointB");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		obj.position = Vector3.Lerp (pointA.position, pointB.position, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+	}
+}
