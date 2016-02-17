974edafdb6b460020dad158f662bf2310f88aec7
JamesClow <jclow@tendrilinc.com>
2016-02-17 11:28:28 -0700

    Day 2 or 3 upload + room edge particle effect

---
+++ CapstoneFreeRunner/Assets/Scripts/Rooms/RoomEdge.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomEdge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float radius = gameObject.GetComponent<ParticleSystem>().shape.radius;
+        gameObject.GetComponent<ParticleSystem>().emissionRate = radius * 1.5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
