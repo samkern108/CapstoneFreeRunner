9234a64f0a4ffb8f435f4b6c2355cc4c6db33e3a
JamesClow <jclow@tendrilinc.com>
2016-03-09 11:31:49 -0700

    day 1 and stuff to make day 1 work

--- CapstoneFreeRunner/Assets/Scripts/Rooms/TutorialTextZone.cs
+++ CapstoneFreeRunner/Assets/Scripts/Rooms/TutorialTextZone.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class TutorialTextZone : MonoBehaviour {
 
-	public HintType hintType = HintType.warp;
+    //public HintType hintType = HintType.warp;
+    public GameObject hint;
 
 	public void OnTriggerEnter2D(Collider2D col) {
 		if (col.CompareTag ("Player")) {
-			TutorialText.self.DisplayTutorialText (hintType);
+            //TutorialText.self.DisplayTutorialText (hintType);
+            hint.SetActive(true);
 		}
 	}
 
 	public void OnTriggerExit2D(Collider2D col) {
 		if (col.CompareTag ("Player")) {
-			TutorialText.self.SquelchTutorialText ();
+            //TutorialText.self.SquelchTutorialText ();
+            hint.SetActive(false);
 		}
 	}
 }
