6ae1709f4f77e710b2164e193923b9b3dbe5010c
Sam Kern <samkern108@gmail.com>
2016-04-16 18:29:23 -0600

    Removed useless materials, changed Day1 aesthetically, and added distortion to the edges of backgrounds. Added a camera background.

---
+++ CapstoneFreeRunner/Assets/Background.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	public bool up;
+	public bool down;
+	public bool left;
+	public bool right;
+	public bool exterior;
+	private int exteriorI = 1;
+
+	public GameObject edgeMeshPrefab;
+	private GameObject edgeMesh;
+
+	void Start () {
+		Bounds bounds = this.GetComponent <SpriteRenderer> ().bounds;
+		if (exterior)
+			exteriorI *= -1;
+
+		if (up) {
+			InstantiateEdgeMesh (bounds.size.x, new Vector3 (bounds.min.x, bounds.max.y, -1), 0);
+		}
+		if (down) {
+			InstantiateEdgeMesh (bounds.size.x, new Vector3 (bounds.max.x, bounds.min.y, -1), 180);
+		}
+		if (left) {
+			InstantiateEdgeMesh (bounds.size.y, new Vector3 (bounds.min.x, bounds.min.y, -1), 90);
+		}
+		if (right) {
+			InstantiateEdgeMesh (bounds.size.y, new Vector3 (bounds.max.x, bounds.max.y, -1), 270);
+		}
+	}
+
+	private void InstantiateEdgeMesh(float length, Vector3 startPos, float angle)
+	{
+		edgeMesh = Instantiate (edgeMeshPrefab);
+		edgeMesh.transform.localPosition = startPos;
+		edgeMesh.transform.Rotate (0,0,angle);
+		edgeMesh.GetComponent <EdgeMesh> ().SetUpMesh (length, exteriorI);
+		edgeMesh.transform.parent = this.transform;
+
+		Vector3 t = edgeMesh.transform.localPosition;
+		t.z = -1;
+		edgeMesh.transform.localPosition = t;
+	}
+}
---
+++ CapstoneFreeRunner/Assets/EdgeMesh.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeMesh : MonoBehaviour {
+
+	private Vector3[] vertices;
+	private int[] triangles;
+	private Mesh mesh;
+
+	private Vector3[] AddVertices(float length)
+	{
+		vertices = new Vector3[Mathf.CeilToInt(length) + 3];
+
+		vertices [0] = new Vector3 (0, 0, 0);
+		vertices [vertices.Length - 1] = new Vector3 (length, 0, 0);
+
+		for (int i = 1; i < vertices.Length - 1; i++) {
+			vertices [i] = new Vector3 (i - 1, 0, 0);
+		}
+
+		vertices [vertices.Length - 2] = new Vector3 (length, 0, 0);
+
+		return vertices;
+	}
+
+	private int[] Triangulate(Vector3[] vertices)
+	{
+		triangles = new int[(vertices.Length-2)*3];
+
+		for(int i = 0, j = 0; i < (vertices.Length-2)*3; i+=3, j++) {
+			triangles[i] = j;
+			triangles[i+1] = j+1;
+			triangles[i+2] = j+2;
+		}
+		return triangles;
+	}
+
+	private float exteriorI;
+
+	public void SetUpMesh (float length, float exteriorI) {
+		this.exteriorI = exteriorI;
+		//Vector3 t = transform.position;
+		//t.z = -1;
+		//transform.position = t;
+
+		vertices = AddVertices (length);
+
+		MeshRenderer renderer=gameObject.AddComponent<MeshRenderer>();
+		renderer.material=new Material(Shader.Find("Hidden/Internal-Colored"));
+		renderer.material.color=Color.black;
+
+		MeshFilter filter=gameObject.AddComponent<MeshFilter>();
+		mesh=new Mesh();
+		mesh.vertices = vertices;
+		mesh.triangles = Triangulate(vertices);
+
+		mesh.RecalculateNormals();
+		mesh.Optimize();
+		filter.mesh=mesh;
+	}
+
+	void Update () {
+		for (int i = 1; i < vertices.Length - 1; i+=2) {
+			this.vertices [i].y = vertices [i].y.Map (0, 8, exteriorI*(-.5f), exteriorI*.5f) * Random.value;
+		}
+
+		mesh.vertices = this.vertices;
+	}
+
+	void OnBecameVisible() {
+		Debug.Log ("Vis");
+		this.enabled = true;
+	}
+
+	void OnBecameInvisible() {
+		Debug.Log ("Invis");
+		this.enabled = false;
+	}
+}
